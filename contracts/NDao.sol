207697e2c711e54d9b09f3259eb722e83cb57176
lil69ye <arnab@0xytocin.com>
2022-02-06 11:58:02 +0530

    First Files

---
+++ contracts/NDao.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+import "./base/ERC20.sol";
+//contracts/base/Base64.sol
+contract NDao is ERC20{
+
+    constructor () ERC20 ("Nature_Dao","ND") {}
+
+    function _mint() external {
+
+    }
+
+}
