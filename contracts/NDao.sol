8f4b452a2dbfb8e2d6b065e507ba1e3f872b307a
lil69ye <arnab@0xytocin.com>
2022-02-06 12:18:26 +0530

    Added initial Mint distribution

--- contracts/NDao.sol
+++ contracts/NDao.sol
@@ -3,10 +3,30 @@ import "./base/ERC20.sol";
 //contracts/base/Base64.sol
 contract NDao is ERC20{
 
-    constructor () ERC20 ("Nature_Dao","ND") {}
+    address public generalInvestors;
+    address public communityTreasury;
+    address public coreTeam;
+    address public advisors;
+    address public auditors;
+    // setters required
 
-    function _mint() external {
-
+    constructor () ERC20 ("Nature_Dao","ND") {
+        _totalSupply = 10 ** 10; //setting up totalSupply as 10 billion
+        initialMinter(10000000); //minting 10 million tokens to different parties
     }
+
+    function initialMinter(uint _amount) internal {
+            uint _giValue = 40*_amount/100;
+            _mint(generalInvestors,_giValue);
+            uint _ctValue = 30 * _amount / 100;
+            _mint (communityTreasury , _ctValue);
+            uint _teamValue = 20 * _amount/100;
+            _mint (coreTeam, _teamValue);
+            uint _advisorValue = 5* _amount / 100;
+            _mint(advisors, _advisorValue);
+            _mint(auditors, _advisorValue);
+    }
+
+
 
 }
