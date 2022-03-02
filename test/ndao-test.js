da991816472b596783e638013660cef3e15539ab
Anik Ghosh <itsmenick30@gmail.com>
2022-03-02 20:33:56 +0530

    test

---
+++ test/ndao-test.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
+const {BN,expectEvent,expectRevert, ether} = require('@openzeppelin/test-helpers');
+const {address} = require("hardhat/internal/core/config/config-validation");
+
+describe('NDAO ICO Testing', async function(){
+    let owner, ico, ndao, anyx;
+    beforeEach("Pre-requisites", async function(){
+        [owner] = await ethers.getSigners();
+        const Ndao = await ethers.getContractFactory("NDAO");
+        const testCoin = await ethers.getContractFactory("Anyx");
+        const NdaoICO = await ethers.getContractFactory("NDAOICO");
+        ndao = await Ndao.deploy();
+        anyx = await testCoin.deploy();
+        ico = await NdaoICO.deploy(await anyx.address, await ndao.address);
+        await anyx.connect(owner).approve(ico.address, ethers.utils.parseEther('100000'));
+    })
+
+    it('Test: Buying NDAO tokens', async function(){
+        await ico.connect(owner).Invest(5);
+    });
+});
